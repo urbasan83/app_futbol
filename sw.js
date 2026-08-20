c3b00f6c52a9f1a270ea99bfadaa348a01751b69
urbasan83 <fotosytalurbsan83@gmail.com>
2026-08-20 17:46:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = https://mpsgwqjpijqmyaqbgtsg.supabase.co/rest/v1/;
+const SUPABASE_URL = https://mpsgwqjpijqmyaqbgtsg.supabase.co;
 const SUPABASE_KEY = sb_publishable_SVQxZ1Vvd5KQVb5xcRfuIQ_frOQtVzf;
 
 const supabase = window.supabase.createClient(
